e0e371f4e21e0dd7707332eb2d858e2710d0bfb8
Natalina-Yarosh <natali.yarosh.96@mail.ru>
2023-10-09 22:22:58 +0300

    change vite

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
-
-module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/cv/'
     : '/'
-}
+})
